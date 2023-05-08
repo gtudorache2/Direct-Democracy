fefd75ff93491bbbe0cb46c4e5ab7f8f9fa65a82
Tudorache Gabriel <gtudorache@gmail.com>
2023-05-08 06:07:32 +0300

    Cleanup and reorganization ...

--- public_html/index.php
+++ public_html/index.php
@@ -762,8 +762,7 @@
   <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/web3@1.2.11/dist/web3.min.js"></script>
-  <script src="platform.js"></script>
-  <script src="taxesPlatform.js" defer></script>
+
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
   <script>
